bcaa078038c33c96c3a2bbae06268d10e8e097a2
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-02-16 23:22:27 +0200

    animate every word 1 by 1

---
+++ app/js/wordAnimation.js
@@ -0,0 +1,23 @@
+(function () {
+
+  const calcDelay = (i, j) => 200 * (i + 2) * (j + 2)
+
+  const splitRows = elem => elem.innerText.split(/\r\n|\r|\n/)
+
+  const insert = (elem, wrappedWords) => elem.innerHTML = wrappedWords.join("\n")
+
+  const animateWords = elem => {
+    const wrappedWords = splitRows(elem).map((row, i) => 
+      row.split(" ")
+      .map((word, j) => 
+        `<span data-aos="fade" data-aos-once="true" data-aos-anchor=".header__name"  data-aos-offset="400" data-aos-delay="${calcDelay(i, j)}">${word}</span>`
+      )
+      .join(" ")
+    )
+
+    insert(elem, wrappedWords)
+  }
+
+  animateWords(document.querySelector(".header__about"))
+
+})()
