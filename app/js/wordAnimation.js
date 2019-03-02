40485eff1337d6c588b49d6bb910f7d5b349c0ab
Edward-Fedoruk <ed.fedorukk@gmail.com>
2019-03-02 15:02:56 +0200

    animate on load

--- app/js/wordAnimation.js
+++ app/js/wordAnimation.js
@@ -4,16 +4,17 @@
 
   const splitRows = elem => elem.innerText.split(/\r\n|\r|\n/)
 
-  const insert = (elem, wrappedWords) => elem.innerHTML = wrappedWords.join("\n")
+  const insert = (elem, wrappedWords) => elem.innerHTML = wrappedWords.join("<br>")
 
   const animateWords = elem => {
-    const wrappedWords = splitRows(elem).map((row, i) => 
-      row.split(" ")
-      .map((word, j) => 
-        `<span data-aos="fade" data-aos-once="true" data-aos-anchor=".header__name"  data-aos-offset="400" data-aos-delay="${calcDelay(i, j)}">${word}</span>`
-      )
-      .join(" ")
-    )
+    const wrappedWords = splitRows(elem).map((row, i) => {
+      return row
+        .split(" ")
+        .map((word, j) => 
+          `<span data-aos="fade" data-aos-once="true" data-aos-anchor=".header__name"  data-aos-offset="400" data-aos-delay="${calcDelay(i, j)}">${word}</span>`
+        )
+        .join(" ")
+    })
 
     insert(elem, wrappedWords)
   }
